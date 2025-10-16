5bcd377004f344797a9ce4bb46ed7b1217e70f25
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-16 19:37:26 -0300

    Cambios a Ver si se Ven

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Acá hay que empezar a escirbir</Text>
       <StatusBar style="auto" />
     </View>
   );
