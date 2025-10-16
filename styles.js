5f8bd379d863036defe68b9628f217198ba64538
Gaspar Berner <gasparberner@gmail.com>
2025-10-16 19:50:50 -0300

    archivo styles

---
+++ styles.js
@@ -0,0 +1,62 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create ({
+
+  container:{
+    flex:1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+
+  input:{
+    flex:1,
+    borderColor:"lightgray",
+    borderRadius: 8,
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 8
+  },
+
+  inputRow:{
+    marginBottom: 12,
+    flexDirection: "row"
+  },
+
+  header:{
+    marginBottom: 8,
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+
+  deleteButton: {
+    fontWeight: "bold",
+    marginRight: 8,
+    color: "red"
+  },
+
+  countButton: {
+    fontSize:20,
+    marginHorizontal: 8
+  },
+
+  countText:{
+    width: 30,
+    textAlign: "center"
+  },
+
+  habitItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 8,
+    backgroundColor: "gray",
+    padding: 8,
+    marginBottom: 8
+  },
+
+  habitName:{
+    flex:1
+  },
+
+});
+
+export default styles;
