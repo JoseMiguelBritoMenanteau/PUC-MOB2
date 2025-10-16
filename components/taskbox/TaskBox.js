0fe899228e4ff2ae1925a26c2470eb8eb58109db
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-16 20:42:09 -0300

    Avance TaskBox

--- components/taskbox/TaskBox.js
+++ components/taskbox/TaskBox.js
@@ -13,15 +13,37 @@ export default function TaskBox() {
         <View style = {stylesTaskBox.container}>
             <Pressable
                 title = 'X'
-                onPress = {() => Alert.alert.alert('Eliminar tarea')} />
-            <Text></Text>
-            <Pressable
-                title = '-'
-                onPress = {() => Alert.alert.alert('Bajar')} />
+                onPress = {() => Alert.alert.alert('Eliminar tarea')}
+                style = {stylesTaskBox.pressable_bin}>
+
+                <Text>
+                    X
+                </Text>
+
+            </Pressable>
+
             <Text>Primera Tarea</Text>
             <Pressable
+                title = '-'
+                onPress = {() => Alert.alert.alert('Bajar')}
+                style = {stylesTaskBox.pressable_bajar}>
+
+                <Text>
+                    -
+                </Text>
+
+            </Pressable>
+            <Text>0</Text>
+            <Pressable
                 title = '+'
-                onPress = {() => Alert.alert.alert('Subir')}  />
+                onPress = {() => Alert.alert.alert('Subir')}
+                style = {stylesTaskBox.pressable_subir}>
+
+                <Text>
+                    +
+                </Text>
+
+            </Pressable>
         </View>
     );
 };
--- components/taskbox/styles-TaskBox.js
+++ components/taskbox/styles-TaskBox.js
@@ -5,11 +5,35 @@ const stylesTaskBox = StyleSheet.create ({
     container: {
         display: 'flex',
         flexDirection: 'row',
+        width: '80%',
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
-    }
+    pressable_bin: {
+        backgroundColor: 'red',
+        width: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    pressable_bajar: {
+        backgroundColor: 'grey',
+        width: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    pressable_subir: {
+        backgroundColor: 'lightblue',
+        width: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 
 
-
+export default stylesTaskBox;
 
